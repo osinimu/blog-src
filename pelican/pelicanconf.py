53be18da2f572a5322baa0affa7f84e94bf37980
fosipo <christopherajulo@gmail.com>
2016-03-21 03:54:01 +0100

    theme changed

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -4,9 +4,38 @@ from __future__ import unicode_literals
 
 AUTHOR = u'fosi'
 SITENAME = u'osisieke'
-SITEURL = ''
+DESCRIPTION = u"LifePages    <strong>\\</strong>"
+SITE_TAGLINE = DESCRIPTION
+SITEURL = 'http://osisieke.github.io'
+MAIL = "christopherajulo@gmail.com"
 
-PATH = 'content'
+BIO = \
+	u'''
+		<p>Full name: <strong>{name}</strong>.</p>
+		
+		<p>
+		</p>
+		<p>I enjoy reading. I appreciate writing; and I try to be good at this myself. Some other interests include singing, technology, and psychology.
+		</p>
+		<p>In my spare time, I listen to podcasts about different topics ranging from startups to technology, innovations, psychology and more.
+		<br/>
+		<br/><b>Email: </b> christopherajulo [at] gmail [dot] com
+		</p>
+	'''
+	
+AUTHOR_SHORTBIO = u''
+AUTHOR_EMAIL = u'christopherajulo@gmail.com'
+AUTHOR_EMAIL_HASH = md5(AUTHOR_EMAIL).hexdigest()
+TWITTER_USERNAME = u'_fosi'
+GITHUB_USERNAME = u'osisieke'
+
+# During development, we want urls to be relative
+RELATIVE_URLS = True
+
+DEFAULT_LANG = u'en'
+
+
+PATH = 'content/'
 
 TIMEZONE = 'Africa/Lagos'
 
@@ -19,17 +48,45 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+
+FILENAME_METADATA='(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+
+
+
+SOCIAL_WIDGET_NAME = "I am on"
+LINKS_WIDGET_NAME = "Bookmarks"
+
+GRAB_ICONS = True
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#       ('Python.org', 'http://python.org/'),
+#      ('Jinja2', 'http://jinja.pocoo.org/'),
+#     ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+        ('Twitter', 'http://twitter.com/_fosi'),
+        ('goodreads', 'http://goodreads.com/user/show/41177369-tosin-damilare-james-animashaun'),
+        ('github', 'http://github.com/osisieke'),
+          )
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 200
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# Settings for current theme
+THEME = 'dopetrope'
+
+MENUITEMS = (
+	('Home', '/'),
+	('About', '#about')
+)
+
+ABOUT_TEXT = u'What is all these about'
+ABOUT_IMAGE =  ""
+ABOUT_LINK = "Find out more"
+
+SITESUBTITLE = SITE_TAGLINE
+
+COPYRIGHT = "Osisieke"
+
+
