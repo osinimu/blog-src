cc95a86e30789c8b956df1371830ba27ed701272
fosipo <christopherajulo@gmail.com>
2016-05-19 18:25:39 +0200

    adding features

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -38,6 +38,9 @@ THEME_STATIC_DIR = 'theme'
 
 USE_FOLDER_AS_CATEGORY = True
 
+SHOW_ARTICLE_AUTHOR = True
+BOOTSTRAP_THEME = "simplex"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -49,10 +52,7 @@ TWITTER_CARDS = True
 
 CC_LICENSE = "CC-BY-NC-SA"
 
-
 FILENAME_METADATA='(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
-
-
 
 SOCIAL_WIDGET_NAME = "I am on"
 LINKS_WIDGET_NAME = "Bookmarks"
@@ -71,6 +71,8 @@ SOCIAL = (
         ('Goodreads', 'http://goodreads.com/user/'),
           )
 		  
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["tipue_search"]
 		  
 DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'authors', 'archives', 'search'))
 
@@ -83,6 +85,7 @@ THEME = u"pelican-bootstrap3"
 
 SHOW_ARTICLE_AUTHOR = True
 SHOW_ARTICLE_CATEGORY = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 
 MENUITEMS = (
 	('Home', '/'),
@@ -93,17 +96,20 @@ BOOTSTRAP_FLUID = True
 
 FAVICON = 'images/favicon.png'
 
-DISPLAY_ARTICLE_INFO_ON_INDEX = False
-
 ABOUT_ME = u''
 
 
+
 DISQUS_NO_ID = True
+USE_OPEN_GRAPH = True 
+TWITTER_CARDS = True
 
-DISQUS_DISPLAY_COUNTS = True
+SHARIFF = True
+SHARIFF_LANG = u'en' 
+SHARIFF_ORIENTATION = u'vertical'
 
-
-SITESUBTITLE = SITE_TAGLINE
+SHARIFF_THEME = u'standard'
+SHARIFF_TWITTER_VIA = True
 
 ARTICLE_URL = 'blog/{slug}.html'
 ARTICLE_SAVE_AS = 'blog/{slug}.html'
@@ -114,7 +120,6 @@ TAG_SAVE_AS = 'tags/{slug}.html'
 TAGS_URL = 'tags.html'
 CATEGORY_URL = ('category/{slug}/')
 CATEGORY_SAVE_AS = ('category/{slug}/index.html')
-PLUGIN_PATHS = ["pelican-plugins"]
-PLUGINS = ["tipue_search"]
+
 
 
